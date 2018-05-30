da6d0add2c5a20830e44c5c3584a68f69f331912
NikhilBoriwaleKshatriya <38933949+NikhilBoriwaleKshatriya@users.noreply.github.com>
2018-05-30 16:43:54 +0530

    Initial Commit

---
+++ AudioVideoDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioVideoDemo
+//
+//  Created by nikhil boriwale on 5/30/18.
+//  Copyright © 2018 infostretch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
